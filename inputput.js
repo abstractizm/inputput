eca31652b48dbed5f194c225d216ca2b87f198e4
abstractizm <abstractizm@googlemail.com>
2011-12-02 13:56:09 +0100

    Main Snippet

---
+++ inputput.js
@@ -0,0 +1,51 @@
+// Author: Hakan Yilmaz
+// E-Mail: abstractizm@googlemail.com
+
+jQuery(document).ready(function($) {
+	    
+    // Input Felder
+    $("input").each(function (index, domEle) {
+		// Abfrage, ob es ein Eingabefeld ist type=text oder textarea
+		if($(domEle).attr('type') == 'text'){
+		    // dem Objekt den Abfrage Wert hinzufügen
+		    $(domEle).attr('defaultWert', $(domEle).attr('value'));
+		}
+		
+		// Listener setzten zum Löschen
+		$(domEle).focus(function(){
+		    if($(domEle).val()==$(domEle).attr('defaultWert')){
+			$(domEle).attr('value','');
+		    }
+		});
+		
+		// Listener setzten zum Wiederherstellen
+		$(domEle).blur(function(){
+		    if($(domEle).val()==''){
+			$(domEle).attr('value',$(domEle).attr('defaultWert'));
+		    }
+		});
+    });
+    
+    // Textarea
+    $("textarea").each(function (index, domEle) {
+		// Abfrage, ob es ein Eingabefeld ist type=text oder textarea
+		// dem Objekt den Abfrage Wert hinzufügen
+		$(domEle).attr('defaultWert', $(domEle).attr('value'));
+	
+		
+		// Listener setzten zum Löschen
+		$(domEle).focus(function(){
+		    if($(domEle).val()==$(domEle).attr('defaultWert')){
+			$(domEle).attr('value','');
+		    }
+		});
+		
+		// Listener setzten zum Wiederherstellen
+		$(domEle).blur(function(){
+		    if($(domEle).val()==''){
+			$(domEle).attr('value',$(domEle).attr('defaultWert'));
+		    }
+		});
+    });
+    
+});
